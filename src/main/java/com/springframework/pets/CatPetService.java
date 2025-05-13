d41e98b8c77211d463d6cda04404b7bfb225ee10
LukasSchweigler <lukas.schweigler@outlook.com>
2025-05-13 13:10:31 +0200

    Samaritenbund

--- src/main/java/com/springframework/pets/CatPetService.java
+++ src/main/java/com/springframework/pets/CatPetService.java
@@ -1,11 +1,9 @@
-package guru.springframework.sfgdi.services;
+package com.springframework.pets;
 
-import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Service("cat")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
